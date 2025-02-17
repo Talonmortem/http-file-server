46e928b4a1cba165afc36618de1cb3344bcbda6d
Talonmortem <oleg48g@gmail.com>
2025-02-17 20:32:00 +0300

    list and del cmd's

--- cmd/del_user.go
+++ cmd/del_user.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/Talonmortem/http-file-server/internal/config"
 	"github.com/Talonmortem/http-file-server/internal/database"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 //TODO: del user
@@ -28,23 +26,17 @@ func main() {
 	defer database.DB.Close()
 
 	// Запрашиваем логин и пароль
-	if len(os.Args) < 3 {
-		fmt.Println("Использование: go run cmd/add_user.go <username> <password>")
+	if len(os.Args) < 2 {
+		fmt.Println("Использование: go run cmd/del_user.go <username>")
 		return
 	}
-	username, password := os.Args[1], os.Args[2]
+	username := os.Args[1]
 
-	// Хэшируем пароль
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	// Удаляем пользователя в БД
+	_, err = database.DB.Exec("DELETE FROM users WHERE username = ?", username)
 	if err != nil {
-		log.Fatal("Ошибка хеширования пароля:", err)
+		log.Fatal("Ошибка удаления пользователя:", err)
 	}
 
-	// Добавляем пользователя в БД
-	_, err = database.DB.Exec("INSERT INTO users (username, password_hash) VALUES (?, ?)", username, string(hash))
-	if err != nil {
-		log.Fatal("Ошибка добавления пользователя:", err)
-	}
-
-	fmt.Println("✅ Пользователь добавлен:", username)
+	fmt.Println("✅ Пользователь удалён:", username)
 }
--- cmd/list_users.go
+++ cmd/list_users.go
@@ -1,2 +1,41 @@
-//TODO: list users
-//TEST
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Talonmortem/http-file-server/internal/config"
+	"github.com/Talonmortem/http-file-server/internal/database"
+)
+
+//TODO: del user
+
+func main() {
+	// Загружаем конфиг
+	cfg, err := config.LoadConfig("configs/config.yaml")
+	if err != nil {
+		log.Fatal("Ошибка загрузки конфигурации:", err)
+	}
+
+	// Подключаемся к БД
+	err = database.ConnectDB(cfg)
+	if err != nil {
+		log.Fatal("Ошибка подключения к БД:", err)
+	}
+	defer database.DB.Close()
+
+	// Запрашиваем логин и пароль
+	if len(os.Args) < 2 {
+		fmt.Println("Использование: go run cmd/list_users.go")
+		return
+	}
+
+	// Удаляем пользователя в БД
+	users, err := database.DB.Exec("SELECT * FROM users")
+	if err != nil {
+		log.Fatal("Ошибка поиска пользователя:", err)
+	}
+
+	fmt.Println("✅ Пользователи:", users)
+}
