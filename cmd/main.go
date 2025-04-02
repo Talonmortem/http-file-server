e3efd6700812cb72c0ba4f9ff6fedc850ec4e73a
Talonmortem <oleg48g@gmail.com>
2025-04-02 17:40:46 +0300

    get port with ENV

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/Talonmortem/http-file-server/internal/config"
 	"github.com/Talonmortem/http-file-server/internal/database"
@@ -16,6 +17,8 @@ func main() {
 	if err != nil {
 		log.Fatal("Не удалось загрузить конфигурацию")
 	}
+
+	cfg.Server.Port, _ = strconv.Atoi(os.Getenv("PORT"))
 
 	if err := os.MkdirAll(cfg.Storage.UploadDir, os.ModePerm); err != nil {
 		log.Fatalf("Не удалось создать директорию для загрузок: %v", err)
